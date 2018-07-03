c954b9706c26df133b0b9167c883bc9bbdb6a63d
Steffan Sluis <steffansluis@gmail.com>
2018-07-03 15:41:15 +0200

    Add painless script to fix encoding field

--- migrations/1_use-aliases.js
+++ migrations/1_use-aliases.js
@@ -3,53 +3,57 @@ const ElasticSearch = require('../lib/elasticsearch');
 
 const indices = [ "resources", "autocomplete" ];
 
+async function indicesExist() {
+	const names = indices
+	.map(indexName => [ `${Config.ELASTICSEARCH_INDEX_NAME}_${indexName}` ])
+	.reduce((memo, val) => [].concat(memo, val));
+
+	return new Promise((resolve, reject) => {
+		ElasticSearch.client.indices.exists({ index: names }, (err, data) => {
+			if (err) return reject(err);
+			console.log(`Testing index existence for ${names}:`, data);
+			return resolve(data);
+		});
+	})
+}
+
+async function reindex() {
+	return Promise.all(indices.map(async indexName => {
+		const task = await new Promise((resolve, reject) => {
+			const aliasName = `${Config.ELASTICSEARCH_INDEX_NAME}_${indexName}`;
+			const name = `${Config.ELASTICSEARCH_INDEX_NAME}_${indexName}_index`;
+			console.log(`Starting reindex from ${aliasName} to ${name}...`);
+			const params = {
+				waitForCompletion: true,
+				refresh: true, // Refresh index once done
+				body: {
+					conflicts: "proceed", // Don't break the job if one document breaks on reindexing
+					source: {
+						index: aliasName
+					},
+					dest: {
+						index: name,
+						version_type: "internal", // Don't care about the version of the stale doc
+						op_type: "create", // The reindexed data is stale, so no need to update anything, just creating missing data is fine
+					},
+					script: {
+						inline: "if (ctx._source.encoding instanceof Object) {ctx._source.encoding = [ ctx._source.encoding.id ]}",
+						lang: "painless"
+					}
+				}
+			};
+
+			ElasticSearch.client.reindex(params, (err, res) => {
+				if (err) return reject(err);
+				return resolve(res);
+			});
+		});
+
+		return task;
+	}));
+}
+
 exports.migrate = async function(client, done) {
-	async function indicesExist() {
-	  const names = indices
-	    .map(indexName => [ `${Config.ELASTICSEARCH_INDEX_NAME}_${indexName}` ])
-	    .reduce((memo, val) => [].concat(memo, val));
-
-	  return new Promise((resolve, reject) => {
-	    client.indices.exists({ index: names }, (err, data) => {
-	      if (err) return reject(err);
-	      console.log(`Testing index existence for ${names}:`, data);
-	      return resolve(data);
-	    });
-	  })
-	}
-
-  async function reindex() {
-    return Promise.all(indices.map(async indexName => {
-      const task = await new Promise((resolve, reject) => {
-        const aliasName = `${Config.ELASTICSEARCH_INDEX_NAME}_${indexName}`;
-        const name = `${Config.ELASTICSEARCH_INDEX_NAME}_${indexName}_index`;
-        console.log(`Starting reindex from ${aliasName} to ${name}...`);
-        const params = {
-          waitForCompletion: true,
-          refresh: true, // Refresh index once done
-          body: {
-            conflicts: "proceed", // Don't break the job if one document breaks on reindexing
-            source: {
-              index: aliasName
-            },
-            dest: {
-              index: name,
-              version_type: "internal", // Don't care about the version of the stale doc
-              op_type: "create", // The reindexed data is stale, so no need to update anything, just creating missing data is fine
-            }
-          }
-        };
-
-        client.reindex(params, (err, res) => {
-          if (err) return reject(err);
-          return resolve(res);
-        });
-      });
-
-      return task;
-    }));
-  }
-
 	try {
 		const exists = await indicesExist();
 		if (!exists) {
